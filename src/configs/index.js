3370448da10f4f46d0499f491936cbb923214793
lucasgabrielsilva <lucas_gabrielsilva@hotmail.com>
2021-02-09 17:18:44 -0300

    Adiçao de cursos

--- src/configs/index.js
+++ src/configs/index.js
@@ -7,18 +7,39 @@ cursos = [
         parcelas: "10x de R$1.000,00"
     },
     {
-        nome: "Ciência da Computação",
+        nome: "Direito",
         investimento: "R$20.000,00",
         nivel: "Pós-Graduação",
         modalidade: "Híbrido",
         parcelas: "10x de R$2.000,00"
     },
     {
-        nome: "Ciência da Computação",
+        nome: "Educação Física",
         investimento: "R$10.000,00",
         nivel: "Graduação",
         modalidade: "EAD",
         parcelas: "10x de R$1.000,00"
+    },
+    {
+        nome: "Ciência da Computação",
+        investimento: "R$50.000,00",
+        nivel: "Pós-Graduação",
+        modalidade: "Presencial",
+        parcelas: "10x de R$5.000,00"
+    },
+    {
+        nome: "Direito",
+        investimento: "R$5.000,00",
+        nivel: "Graduação",
+        modalidade: "EAD",
+        parcelas: "5x de R$1.000,00"
+    },
+    {
+        nome: "Educação Física",
+        investimento: "R$8.000,00",
+        nivel: "Graduação",
+        modalidade: "Híbrido",
+        parcelas: "8x de R$1.000,00"
     },
 ];
 
