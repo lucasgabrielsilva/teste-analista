1fc3fad30b5c8a0b14e6c9945aaea5d6a5c4d85f
lucasgabrielsilva <lucas_gabrielsilva@hotmail.com>
2021-02-09 15:41:23 -0300

    correçao no codigo de retorno da api

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,19 +3,19 @@ const BuscaCep = require('busca-cep');
 module.exports = {
 	async checkCEP(req, res) {
 		try{
-            BuscaCep(req.body.cep, {sync: false, timeout: 3000}).then(endereco => {
-                return res.status(400).send({status: "Sucess", data: endereco});
-            }).catch(err => {
-                return res.status(400).send({status: "ERROR", msg: err.toString()});
-            });
+      BuscaCep(req.body.cep, {sync: false, timeout: 3000}).then(endereco => {
+        return res.status(200).send({status: "Sucess", data: endereco});
+      }).catch(err => {
+        return res.status(400).send({status: "ERROR", msg: err.toString()});
+      });
 		} catch(err){
 			return res.status(400).send({status: "ERROR", msg: err.toString()});
 		}
     },
     async getCursos(req, res) {
 		try{
-            const cursos = require("../configs");
-            return res.status(400).send({status: "Sucess", data: cursos});
+        const cursos = require("../configs");
+        return res.status(200).send({status: "Sucess", data: cursos});
 		} catch(err){
 			return res.status(400).send({status: "ERROR", msg: err.toString()});
 		}
